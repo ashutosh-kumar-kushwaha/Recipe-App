e7a51bc2c455a2f5f91e20d56bce7f297b2944f9
Ashutosh <akkushwaha0104@gmail.com>
2024-06-27 11:02:28 +0530

    Complete OnboardingScreen UI

--- app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/onboarding/OnboardingScreen.kt
@@ -1,9 +1,98 @@
 package me.ashutoshkk.recipeapp.presentation.ui.onboarding
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.ashutoshkk.recipeapp.R
+import me.ashutoshkk.recipeapp.presentation.ui.theme.RecipeTheme
 
 @Composable
 fun OnboardingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.login_background_image),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillHeight
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(RecipeTheme.paddings.all)
+        ) {
+            Text(
+                text = buildAnnotatedString {
+                    append("Welcome to\n")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Black)) {
+                        append("Rec")
+                    }
+                    append("ii")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Black)) {
+                        append("p")
+                    }
+                    append("ii")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Black)) {
+                        append("e")
+                    }
+                },
+                fontSize = 48.sp,
+                color = RecipeTheme.colorScheme.text2,
+                fontStyle = FontStyle.Italic,
+                lineHeight = 48.sp
+            )
+            Text(
+                text = stringResource(id = R.string.please_signing_to_continue),
+                fontSize = 14.sp,
+                color = RecipeTheme.colorScheme.text2,
+            )
+            Spacer(modifier = Modifier.height(RecipeTheme.paddings.verticalInBetween))
+            Button(
+                onClick = {
 
+                },
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.coolicon),
+                    modifier = Modifier
+                        .height(18.dp)
+                        .padding(end = 10.dp),
+                    contentDescription = null)
+                Text(
+                    text = "Continue with google",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/theme/Theme.kt
@@ -1,33 +1,30 @@
 package me.ashutoshkk.recipeapp.presentation.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 data class ColorScheme(
     val background: Color,
     val onBackground: Color,
+    val text: Color,
+    val text2: Color
 )
 
 private val colorScheme = ColorScheme(
     background = Color.White,
-    onBackground = Color.Black
+    onBackground = Color.Black,
+    text = Color.Black,
+    text2 = Color.White
 )
 
 private val colors = lightColorScheme(
@@ -38,6 +35,7 @@ private val LocalColorScheme = compositionLocalOf { colorScheme }
 
 @Immutable
 data class Paddings(
+    val all: Dp = 20.dp,
     val around: Dp = 8.dp,
     val horizontal: Dp = 16.dp,
     val vertical: Dp = 8.dp,
@@ -69,12 +67,12 @@ fun RecipeAppTheme(
 
 object RecipeTheme {
     val colorScheme: ColorScheme
-    @Composable
-    @ReadOnlyComposable
-    get() = LocalColorScheme.current
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorScheme.current
 
     val paddings: Paddings
-    @Composable
-    @ReadOnlyComposable
-    get() = LocalPaddings.current
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalPaddings.current
 }
