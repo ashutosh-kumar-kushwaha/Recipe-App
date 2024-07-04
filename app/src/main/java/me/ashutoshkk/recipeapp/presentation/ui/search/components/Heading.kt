28a5a5ca9de0e4f8bc7a2fa8d9130de0bf78a775
Ashutosh <akkushwaha0104@gmail.com>
2024-07-04 13:42:06 +0530

    Add drag gesture in Heading

--- app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/search/components/Heading.kt
+++ app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/search/components/Heading.kt
@@ -2,11 +2,13 @@ package me.ashutoshkk.recipeapp.presentation.ui.search.components
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -15,19 +17,67 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import me.ashutoshkk.recipeapp.presentation.ui.theme.RecipeTheme
+import kotlin.math.abs
+import kotlin.math.roundToInt
 
 @Composable
 fun Heading(
     @StringRes name: Int,
     onBackClick: () -> Unit
 ) {
+    var direction by remember { mutableStateOf(Direction.NONE) }
+    var offset by remember { mutableStateOf(IntOffset(0, 0)) }
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .offset { offset }
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDrag = { change, dragAmount ->
+                        change.consume()
+                        if (dragAmount.y > 0) {
+                            offset += IntOffset(0, dragAmount.y.roundToInt())
+                        }
+                        val (x, y) = dragAmount
+                        if (abs(x) > abs(y)) {
+                            when {
+                                x > 0 -> {
+                                    direction = Direction.RIGHT
+                                }
+
+                                x < 0 -> {
+                                    direction = Direction.LEFT
+                                }
+                            }
+                        } else {
+                            when {
+                                y > 0 -> {
+                                    direction = Direction.DOWN
+                                }
+
+                                y < 0 -> {
+                                    direction = Direction.UP
+                                }
+                            }
+                        }
+                    },
+                    onDragEnd = {
+                        if (direction == Direction.DOWN) onBackClick()
+                        offset = IntOffset(0, 0)
+                    }
+                )
+            }
     ) {
         Spacer(
             modifier = Modifier
@@ -61,4 +111,12 @@ fun Heading(
             }
         }
     }
+}
+
+enum class Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    NONE
 }
--- app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/search/components/RecipeBottomSheet.kt
+++ app/src/main/java/me/ashutoshkk/recipeapp/presentation/ui/search/components/RecipeBottomSheet.kt
@@ -3,7 +3,10 @@ package me.ashutoshkk.recipeapp.presentation.ui.search.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +30,12 @@ fun RecipeBottomSheet(
     fetchSimilarRecipe: () -> Unit,
     onDismiss: () -> Unit,
 ) {
+    var step by remember {
+        mutableStateOf(Step.IMAGE)
+    }
+    var back by remember {
+        mutableStateOf(false)
+    }
     ModalBottomSheet(
         modifier = Modifier.fillMaxHeight(0.78f),
         sheetState = rememberModalBottomSheetState(
@@ -38,80 +47,141 @@ fun RecipeBottomSheet(
                 name = recipe.title,
                 isAddedToWaitlist = false,
                 onBackClick = {
-
+                    back = true
+                    when (step) {
+                        Step.IMAGE -> {}
+                        Step.INGREDIENTS -> step = Step.IMAGE
+                        Step.FULL_RECIPE -> step = Step.INGREDIENTS
+                        Step.SIMILAR_RECIPE -> step = Step.FULL_RECIPE
+                    }
                 }
             ) {
 
             }
         }
     ) {
-        var step by remember {
-            mutableStateOf(Step.IMAGE)
-        }
         Column(
             modifier = Modifier
-
         ) {
             AnimatedVisibility(
                 visible = step == Step.IMAGE,
             ) {
                 ImageStep(recipe = recipe) {
+                    back = false
                     step = Step.INGREDIENTS
                 }
             }
             AnimatedVisibility(
                 visible = step == Step.INGREDIENTS,
-                enter = slideInVertically(
-                    tween(1500),
-                    initialOffsetY = { 1980 }
-                )
+                enter = if (back) {
+                    fadeIn(tween(1))
+                } else {
+                    slideInVertically(
+                        tween(1500),
+                        initialOffsetY = { 1980 }
+                    )
+                },
+                exit = if(back) {
+                    slideOutVertically(
+                        tween(1500),
+                        targetOffsetY = { 1980 }
+                    )
+                } else {
+                    fadeOut(tween(1))
+                }
             ) {
                 IngredientsStep(
                     recipe.ingredients,
                     onBackClick = {
+                        back = true
                         step = Step.IMAGE
                     }
                 ) {
+                    back = false
                     step = Step.FULL_RECIPE
                 }
             }
-            if (step == Step.FULL_RECIPE || step == Step.SIMILAR_RECIPE) {
+            AnimatedVisibility(
+                step == Step.FULL_RECIPE || step == Step.SIMILAR_RECIPE,
+                enter = fadeIn(tween(1)),
+                exit = if(back) {
+                    slideOutVertically(
+                        tween(1500),
+                        targetOffsetY = { 1980 }
+                    )
+                } else {
+                    fadeOut(tween(1))
+                }
+            ) {
                 Heading(R.string.ingredients) {
+                    back = true
                     step = Step.IMAGE
                 }
             }
             AnimatedVisibility(
                 visible = step == Step.FULL_RECIPE,
-                enter = slideInVertically(
-                    tween(1500),
-                    initialOffsetY = { 1980 }
-                )
+                enter = if (back) {
+                    fadeIn(tween(1))
+                } else {
+                    slideInVertically(
+                        tween(1500),
+                        initialOffsetY = { 1980 }
+                    )
+                },
+                exit = if(back) {
+                    slideOutVertically(
+                        tween(1500),
+                        targetOffsetY = { 1980 }
+                    )
+                } else {
+                    fadeOut(tween(1))
+                }
             ) {
                 FullRecipeStep(
                     recipe,
                     onBackClick = {
+                        back = true
                         step = Step.INGREDIENTS
                     }
                 ) {
+                    back = false
                     fetchSimilarRecipe()
                     step = Step.SIMILAR_RECIPE
                 }
             }
-            if (step == Step.SIMILAR_RECIPE) {
+            AnimatedVisibility(
+                visible = step == Step.SIMILAR_RECIPE,
+                enter = fadeIn(tween(1)),
+                exit = fadeOut(tween(1))
+            ) {
                 Heading(R.string.full_recipe) {
+                    back = true
                     step = Step.INGREDIENTS
                 }
             }
             AnimatedVisibility(
                 visible = step == Step.SIMILAR_RECIPE,
-                enter = slideInVertically(
-                    tween(1500),
-                    initialOffsetY = { 1980 }
-                )
+                enter = if (back) {
+                    fadeIn(tween(1))
+                } else {
+                    slideInVertically(
+                        tween(1500),
+                        initialOffsetY = { 1980 }
+                    )
+                },
+                exit = if(back) {
+                    slideOutVertically(
+                        tween(1500),
+                        targetOffsetY = { 1980 }
+                    )
+                } else {
+                    fadeOut(tween(1))
+                }
             ) {
                 SimilarRecipeStep(
                     similarRecipe,
                 ) {
+                    back = true
                     step = Step.FULL_RECIPE
                 }
 
